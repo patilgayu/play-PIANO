a7f91629def8432e67d98e61af6ec7eadbfddaad
patilgayu <gayatrivpatil16@gmail.com>
2024-03-01 00:51:56 +0530

    tunes added

--- paino.js
+++ paino.js
@@ -3,7 +3,7 @@ volumeSlider = document.querySelector(".volume-slider input"),
 keysCheckbox = document.querySelector(".keys-checkbox input");
 
 let allKeys = [],
-audio = new Audio(`tunes/a.wav`); // by default, audio src is "a" tune
+audio = new Audio("tunes/a.wav"); // by default, audio src is "a" tune
 
 const playTune = (key) => {
     audio.src = `tunes/${key}.wav`; // passing audio src based on key pressed 
